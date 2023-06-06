548045a77959e05dc2853cc64c8e28a36ab56f12
Obecalp <124152468+Obecalp@users.noreply.github.com>
2023-06-06 17:42:36 +0200

    HLA_allele fixed

--- HLA.sh
+++ HLA.sh
@@ -1,11 +1,21 @@
-	#!/bin/bash
+#!/bin/bash
 
-# Cherche les allèles correspondant à l'identifiant $i dans le fichier $2, en distinguant les receveur (R) des donneurs (D) et les alleles en fonction de leur position dans le tableau ((A=4, DQ=13), refomante les alleles **:** en ****, puis le stock dans la variable correspondante. X1 et X2 proviennent du donneur, X3 et X4 du receveur.
+
+function compare(){
+	if [ $1 -eq $2 ] && [ $3 -eq $4 ] && [ $1 -eq $3 ] 
+	then
+		echo 0
+	else
+		echo $((($(echo "$1" | grep "-E" "$3|$4" | uniq | wc "-l")+$(echo "$2" | grep "-E"  "$3|$4" | grep "-vE" "$(echo "$1" | grep "-E" "$3|$4")" | uniq | wc "-l")-2)*-1))
+	fi
+}
+
+#seek alleles corresponding to the id $1 in the file $2, discriminate recipient/donor (R/D) in $3, then select the allele on the position in $4 (A=4,DQ=13)
 function attribution(){
 	grep "-wE" "$1" "$2" | grep ";$3;" | cut '-d' ';' '-f' "$4" | cut '-d' ':' '-f' '1,2' | sed "s/://g"
 	}
 
-#Définition de la variable précision ci dessous, détermine l'argument donné:
+#Define precision:
 function exclusion(){
 if [ $(echo "$1" | grep "$2" | wc "-l") -gt 0 ]
 then
@@ -21,8 +31,11 @@ function score(){
 
 }
 
-#définie avec exclusion()
+#optionnal and defined avec exclusion(), E = don't select "exposed=f" C = don't select "exposed =t" V = don't select "verified = f" P = don't select "verified = t"
+# warning, if two opposite are select simultaneously, no result will be given
 precision=$(exclusion "$3" "E" "|f;[f-t]")$(exclusion "$3" "C" "|t;[f-t]")$(exclusion "$3" "V" "|[f-t];f")$(exclusion "$3" "P" "|[f-t];t")
+
+
 
 #header
 echo "groupe;HLA A;HLA B;HLA C;CLASSE I;HLA DR;HLA DQ;HLA DQA1; HLA DP;CLASSE II;TOTAL;mm_A;mm_B;mm_C;mm_DR;mm_DQ;mm_DQA1;mm_DR;mm_allI;mm_allII;mm_epiA;mm_epiB;mm_epiC;mm_epiDR;mm_epiDQ;mm_epiDQA1;mm_epiDP;A1_1;A2_1;B1_1;B2_1;C1_1;C2_1;DR1_1;DR2_1;DQ1_1;DQ2_1;DQA11_1;DQA2_1;DP1_1;DP2_1;A1_2;A2_2;B1_2;B2_2;C1_2;C2_2;DR1_2;DR2_2;DQ1_2;DQ2_2;DQA11_2;DQA12_2;DP1_2:DP2_2" > "enrocil.csv"
@@ -31,6 +44,7 @@ echo "groupe;HLA A;HLA B;HLA C;CLASSE I;HLA DR;HLA DQ;HLA DQA1; HLA DP;CLASSE II
 for i in $(grep ";" "$1" | grep "-v" "ID_group" | cut '-d' ';' '-f' '1'| uniq)
 do
 
+#execution of attribution to select alleles
 A1=$(attribution "$i" "$1" "D" "4")
 A2=$(attribution "$i" "$1" "D" "5")
 B1=$(attribution "$i" "$1" "D" "6")
@@ -65,44 +79,29 @@ DP3=$(attribution "$i" "$1" "R" "16")
 DP4=$(attribution "$i" "$1" "R" "17")
 
 
-#découpement du calcul du mismatch alleles (alleleA, etc...
-#AD=$(attribution "$i" "$1" "D" "4,5" | sed "s/;/\n/g") => alleles en D
-#AR=$(attribution "$i" "$1" "R" "4,5" | sed "s/;/\n/g") => alleles en R
-#AR=$(echo "$AD" | grep "$AR" | wc "-l") => calcul score en fonction de la difference
-#allele=$((($AR-2)*-1)) #conversion des valeurs
+#Allele mismatches calcul
+alleleA=$(compare "$A1" "$A2" "$A3" "$A4")
+alleleB=$(compare "$B1" "$B2" "$B3" "$B4")
+alleleC=$(compare "$C1" "$C2" "$C3" "$C4")
+alleleDQ=$(compare "$DQ1" "$DQ2" "$DQ4" "$DQ4")
+alleleDR=$(compare "$DR1" "$DR2" "$DR3" "$DR4")
+alleleDQA1=$(compare "$DQA11" "$DQA12" "$DQA13" "$DQA14")
+alleleDP=$(compare "$DP1" "$DP2" "$DP3" "$DP4")
 
-alleleA=$((($(echo "$(attribution "$i" "$1" "D" "4,5" | sed "s/;/\n/g")" | grep "$(attribution "$i" "$1" "R" "4,5" | sed "s/;/\n/g")" | wc "-l")-2)*-1)) #conversion des valeurs
-
-
-alleleB=$((($(echo "$(attribution "$i" "$1" "D" "6,7" | sed "s/;/\n/g")" | grep "$(attribution "$i" "$1" "R" "6,7" | sed "s/;/\n/g")" | wc "-l")-2)*-1)) #conversion des valeurs
-
-
-alleleC=$((($(echo "$(attribution "$i" "$1" "D" "8,9" | sed "s/;/\n/g")" | grep "$(attribution "$i" "$1" "R" "8,9" | sed "s/;/\n/g")" | wc "-l")-2)*-1)) #conversion des valeurs
-
-alleleDR=$((($(echo "$(attribution "$i" "$1" "D" "10,11" | sed "s/;/\n/g")" | grep "$(attribution "$i" "$1" "R" "10,11" | sed "s/;/\n/g")" | wc "-l")-2)*-1)) #conversion des valeurs
-
-alleleDQ=$((($(echo "$(attribution "$i" "$1" "D" "12,13" | sed "s/;/\n/g")" | grep "$(attribution "$i" "$1" "R" "12,13" | sed "s/;/\n/g")" | wc "-l")-2)*-1)) #conversion des valeurs
-
-alleleDQA1=$((($(echo "$(attribution "$i" "$1" "D" "14,15" | sed "s/;/\n/g")" | grep "$(attribution "$i" "$1" "R" "14,15" | sed "s/;/\n/g")" | wc "-l")-2)*-1)) #conversion des valeurs
-
-alleleDP=$((($(echo "$(attribution "$i" "$1" "D" "16,17" | sed "s/;/\n/g")" | grep "$(attribution "$i" "$1" "R" "16,17" | sed "s/;/\n/g")" | wc "-l")-2)*-1)) #conversion des valeurs
 
 alleleI=$(($alleleA+$alleleB+$alleleC))
 alleleII=$((alleleDR+$alleleDQ+alleleDQA1+alleleDP))
 
 
 
+#for an group_ID:
+
+#BaseX: epitopes mismatches listing for allele X 
+#scoreX: count of epitopes mismatches for alleleX
+#epitopeX: used for avoid "\n"
 
 
-
-#pour un même identifiant de groupe:
-
-#BaseX: calcul des mismatch d'epitopes contenus dans la banque de donnée de l'allele X 
-#scoreX: contient le nombre d'épitope
-#epitopeX: nécessaire pour éviter les sauts de case
-
-
-#Stockage des mismatchs d'épitopes dans les variables baseX
+#Mismatches in BaseX
 BaseA=$(score "A;$A1;*|A;$A2;*" "$2" "A;$A3;*|A;$A4;*" "$precision")
 BaseB=$(score "B;$B1;*|B;$B2;*" "$2" "B;$B3;*|B;$B4;*" "$precision")
 BaseC=$(score "C;$C1;*|C;$C2;*" "$2" "C;$C3;*|C;$C4;*" "$precision")
@@ -114,7 +113,7 @@ BaseDP=$(score "DP;$DP1;*|DP;$DP2;*" "$2" "DP;$DP3;*|DP;$DP4;*" "$precision")
 BaseII=$(score "DR;$DR1;*|DR;$DR2;*|DQ;$DQ1;*|DQ;$DQ2;*|DQA1;$DQA11;*|DQA1;$DQA12;*|DP;$DP1;*|DP;$DP2;*" "$2" "DR;$DR3;*|DR;$DR4;*|DQ;$DQ3;*|DQ;$DQ4;*|DQA1;$DQA13;*|DQA1;$DQA14;*|DP;$DP3;*|DP;$DP3;*" "$precision")
 
 
-#Stockage des décomptes dans les variables scoreX
+#Mismatches count in scoreX
 scoreA=$(echo $BaseA | tr ' ' "\n" | grep "-E" "[a-zA-Z0-9]+" | wc "-l")
 scoreB=$(echo $BaseB | tr ' ' "\n" | grep "-E" "[a-zA-Z0-9]+" | wc "-l")
 scoreC=$(echo $BaseC | tr ' ' "\n" | grep "-E" "[a-zA-Z0-9]+" | wc "-l")
@@ -129,7 +128,7 @@ scoreDP=$(echo $BaseDP | tr '? ' "\n" | grep "-E" "[a-zA-Z0-9]+" | wc "-l")
 scoreII=$(echo $BaseII | tr ' ' "\n" | grep "-E" "[a-zA-Z0-9]+" | wc "-l")
 Total=$(($scoreI+$scoreII))
 
-#Listage des epitopes dans les variables epitopeX
+#mismatches listing in epitopeX
 epitopeA=$(echo $BaseA)
 epitopeB=$(echo $BaseB)
 epitopeC=$(echo $BaseC)
@@ -139,7 +138,7 @@ epitopeDQA1=$(echo $BaseDQA1)
 epitopeDP=$(echo $BaseDP)
 
 
-#Rend les résultats sous forme de csv pour chaque id
+#Return result in a csv
 echo "$i;$scoreA;$scoreB;$scoreC;$scoreI;$scoreDR;$scoreDQ;$scoreDQA1;$scoreDP;$scoreII;$Total;$alleleA;$alleleB;$alleleC;$alleleDR;$alleleDQ;$alleleDQA1;$alleleDP;$alleleI;$alleleII;$epitopeA;$epitopeB;$epitopeC;$epitopeDR;$epitopeDQB;$epitopeDQA1;$epitopeDQP;$A1;$A2;$B1;$B2;$C1;$C2;$DR1;$DR2;$DQ1;$DQ2;$DQA11;$DQA12;$DP1;$DP2;$A3;$A4;$B3;$B4;$C3;$C4;$DR3;$DR4;$DQ3;$DQ4;$DQA13;$DQA14;$DP3;$DP4" >> "enrocil.csv"
 done
 
